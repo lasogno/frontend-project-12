243b634cde4a3ca4b936124b0236d2666190ddb6
Alisa Lobanova <17april94@gmail.com>
2022-10-19 18:09:46 +0300

    fix renaming

--- frontend/src/slices/selectors.js
+++ frontend/src/slices/selectors.js
@@ -7,6 +7,6 @@ import { selectors as messagesSelectors } from './messagesSlice';
 export const getAllChannels = (state) => channelsSelectors.selectAll(state);
 export const getAllMessages = (state) => messagesSelectors.selectAll(state);
 export const getCurrentChannelId = (state) => state.channels.currentChannelId;
-export const getModalChannel = (state) => channelsSelectors.selectById(state, state.modals.channelId);
+export const getModalChannel = (state) => channelsSelectors.selectById(state, state.modal.channelId);
 export const getCurrentChannel = createSelector((state) => state, getCurrentChannelId, (state, currentChannelId) => channelsSelectors.selectById(state, currentChannelId));
 export const getcurrentChannelMessages = createSelector([getAllMessages, getCurrentChannelId], (allMessages, currentChannelId) => allMessages.filter(({ channelId }) => channelId === currentChannelId));
